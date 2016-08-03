4922da3d0b059afe81ef3f8d2c7283bded85cea6
budleigh <arguewithplato@gmail.com>
2016-08-03 15:52:31 -0700

    (feat) Add skeleton of user controller

---
+++ src/db/user/userController.js
@@ -0,0 +1,29 @@
+const User = require('./userModel');
+
+User.create = function (email) {
+
+}
+
+User.addActivity = function (activity, day) {
+
+}
+
+User.deleteActivity = function (activity, day) {
+
+}
+
+User.addMetric = function (metric, day) {
+
+}
+
+User.deleteMetric = function (metric, day) {
+
+}
+
+User.saveJournal = function (journal, day) {
+
+}
+
+User.getCorrelations = function (metric) {
+
+}
