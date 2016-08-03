6c6cad517a828f36ce2fce41527d9a2cc62e41d5
budleigh <arguewithplato@gmail.com>
2016-08-03 16:59:57 -0700

    (feat) Initialize the user controller code

--- src/db/user/userController.js
+++ src/db/user/userController.js
@@ -1,29 +1,52 @@
 const User = require('./userModel');
 
-User.create = function (email) {
+function newUser (email) {
+  return User.create({
+    email: email
+  });
+}
 
+function findUser (email) {
+  return User.findOne({
+    email: email
+  });
+}
+
+function removeUser (email) {
+  return User.remove({
+    email: email
+  });
 }
 
 User.addActivity = function (activity, day) {
-
+  this.days[day].activities.push(activity);
+  return this.save();
 }
 
 User.deleteActivity = function (activity, day) {
-
+  this.days[day].activities.splice(
+    this.days[day].activities.indexOf(activity), 1
+  );
+  return this.save();
 }
 
 User.addMetric = function (metric, day) {
-
+  this.days[day].metrics.push(metric);
+  return this.save();
 }
 
 User.deleteMetric = function (metric, day) {
-
+  this.days[day].metrics.splice(
+    this.days[day].metrics.indexOf(metric), 1
+  );
+  return this.save();
 }
 
 User.saveJournal = function (journal, day) {
-
+  this.days[day].journal = journal
+  return this.save();
 }
 
 User.getCorrelations = function (metric) {
-
+  return {}
 }
