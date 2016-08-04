7770c838f51b4b5d3555b5695f0d07944c9b8ed9
Blake Danson <blakedanson@gmail.com>
2016-08-03 17:32:10 -0700

    (feat) Add structure for api routes, test file, and testing dependancies

--- src/server/server.js
+++ src/server/server.js
@@ -3,9 +3,61 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var path = require('path');
+var User = require('../db/user/userController.js');
+var _ = require('underscore');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/../client')));
+
+
+//Routes
+//
+app.route('/user')
+  .get(function(req, res){
+
+  })
+  //Add a user.
+  .post(function(req, res) {
+    //grab email, firstname, lastname from req.body
+
+
+    if (req.body) {
+      console.log(req.body, 'state of data');
+    }
+    var adding = JSON.stringify(Users.create(req.body));
+    res.status(201).send(adding);
+  });
+
+
+
+app.route('/api/users/:id')
+  .get(function(req, res) {
+    var idPath = req.path.split('/');
+    var id = idPath[idPath.length - 1];
+    var userString = JSON.stringify(Users.getOne(id));
+    res.send(userString);
+  })
+  .put(function(req, res) {
+    var idPath = req.path.split('/');
+    var id = idPath[idPath.length - 1];
+    var userString = JSON.stringify(Users.updateOne(id, req.body));
+    res.send(userString);
+  })
+  .delete(function(req, res) {
+    var idPath = req.path.split('/');
+    var id = idPath[idPath.length - 1];
+    var userString = JSON.stringify(Users.deleteOne(id));
+    res.send(userString);
+  });
+
+
+
+
+
+
+
+
 
 var port = process.env.PORT || 3000;
 var databaseUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/readme';
