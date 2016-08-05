b673deef089d39e88cf596e724f8ed99f3adc69e
Blake Danson <blakedanson@gmail.com>
2016-08-05 12:29:36 -0700

    (fix) Add Logout functionality, and keeps the state of tokens upon refresh

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -7,7 +7,7 @@
       'ui.router',
       'auth0',
       'angular-storage',
-      'angular-jwt', 
+      'angular-jwt',
       'ngRoute'
       ]);
 
@@ -40,11 +40,11 @@
 
         //Remove this when testing
         //Angular HTTP Interceptor function
-        // jwtInterceptorProvider.tokenGetter = ['store', function(store) {
-        //     return store.get('token');
-        // }];
-        // //Push interceptor function to $httpProvider's interceptors
-        // $httpProvider.interceptors.push('jwtInterceptor');
+        jwtInterceptorProvider.tokenGetter = ['store', function(store) {
+            return store.get('token');
+        }];
+        //Push interceptor function to $httpProvider's interceptors
+        $httpProvider.interceptors.push('jwtInterceptor');
 
     }]);
 
@@ -70,7 +70,7 @@
           templateUrl: 'app/home/home-states/entries/entries.tmpl.html',
           controller: 'EntriesController',
           controllerAs: 'entries'
-          
+
           // data: { requiresLogin: true }
         })
         .state('home.journal', {
@@ -78,7 +78,7 @@
           templateUrl: 'app/home/home-states/journal/journal.tmpl.html',
           controller: 'JournalController',
           controllerAs: 'journal'
-          
+
           // data: { requiresLogin: true }
         })
         .state('home.insights', {
@@ -86,7 +86,7 @@
           templateUrl: 'app/home/home-states/insights/insights.tmpl.html',
           controller: 'InsightsController',
           controllerAs: 'insights'
-          
+
           // data: { requiresLogin: true }
         })
 
@@ -96,4 +96,23 @@
   // This hooks all auth events to check everything as soon as the app starts
       auth.hookEvents();
     }]);
+
+  app.run(['$rootScope', 'auth', 'store', 'jwtHelper', '$location',
+    function($rootScope, auth, store, jwtHelper, $location) {
+      $rootScope.$on('$locationChangeStart', function() {
+        var token = store.get('token');
+        if (token) {
+          if (!jwtHelper.isTokenExpired(token)) {
+            if (!auth.isAuthenticated) {
+              //Re-authenticate user if token is valid
+              auth.authenticate(store.get('profile'), token);
+            }
+          } else {
+            // Either show the login page or use the refresh token to get a new idToken
+            $location.path('/');
+          }
+        }
+      });
+  }]);
+
 })();
--- src/client/app/landing/landing.js
+++ src/client/app/landing/landing.js
@@ -7,5 +7,13 @@
     /*jshint validthis: true */
     var landing = this;
     $scope.auth = auth;
+
+    $scope.logout = function() {
+      auth.signout();
+      store.remove('profile');
+      store.remove('token');
+      $location.path('/landing.tmpl.html');
+      console.log('Logging Out');
+    };
   }
 })();
