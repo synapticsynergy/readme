4706a709e511fa76ec9c953218f17cca636e78f5
doitlikepruett <doitlikepruett@gmail.com>
2016-08-06 11:23:37 -0700

    (fix) Add Angular Material CSS link to index page

--- src/client/app/home/home-states/entries/entries.js
+++ src/client/app/home/home-states/entries/entries.js
@@ -3,10 +3,11 @@
   angular.module('app.home.entries', []).controller('EntriesController',
     EntriesController);
 
-  function EntriesController($scope, homeFactory) {
+  function EntriesController($scope, $timeout, $q, $log, homeFactory) {
     /*jshint validthis: true */
     var entries = this;
 
+    //Date Picker Variable
     entries.userDate;
 
     entries.autoCompleteDisabled = true;
@@ -15,7 +16,11 @@
       entries.userDate ? entries.autoCompleteDisabled = false : entries.autoCompleteDisabled = true;
     }
 
-    entries.userActivitiesStub = ['run', 'rage', 'slide', 'skip', 'hop', 'hold', 'hodor'];
+    entries.userActivitiesStub = 'run, rage, slide, skip, hop, hold, hodor';
+
+    //AutoComplete Logic
+
+
 
     entries.init = function () {
       getUserData(window.userEmail)
