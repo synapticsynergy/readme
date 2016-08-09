73799b6b746cdb506f6b386a926ee67386102997
doitlikepruett <doitlikepruett@gmail.com>
2016-08-09 16:53:50 -0700

    (fix) Refactor insights controller

--- src/client/app/home/home-states/insights/insights.js
+++ src/client/app/home/home-states/insights/insights.js
@@ -3,32 +3,23 @@
   angular.module('app.home.insights', []).controller('InsightsController',
     InsightsController);
 
-  function InsightsController($scope, homeFactory, $http) {
+  function InsightsController($scope, homeFactory, $http, store) {
     /*jshint validthis: true */
     var insights = this;
 
-    insights.correlationData = 'The data points of your life are not connected in any way. All aspects of your universe exist in separate, unreachable dimensions. Basically, there are no correlations.';
+    insights.userMetrics = store.get('userData').userMetrics;
 
-    insights.userActivities = homeFactory.userActivities;
-    insights.userMetrics = homeFactory.userMetrics;
-
-    insights.userActivitiesStub = ['run', 'rage', 'slide', 'skip', 'hop', 'hold', 'hodor'];
-
-    insights.userMetricsStub = ['headache', 'angry', 'happy', 'sad', 'joyful'];
-
-    insights.currentCorrelationData = [{'run': 0.01}, {'walk': -1}, {'headache': 0.90},{'eat': -0.20},{'sleep': 0.00},{'ascend': 1}];
+    insights.currentCorrelationData = store.get('currentCorrelationData') || [{'Null': 'null'}];
 
     insights.getCorrelations = function(selection) {
-      console.log("getCorrelations selection", selection)
       return $http({
           method: "POST",
           url: '/user/correlation',
-          body: {
+          data: {
             datums: selection
           }
-          
         }).then(function success(resp){
-          console.log("Posted!", resp)
+          console.table(resp.data)
         }, function error(resp){
           console.log("Error!", resp)
           alert('Sorry, there was an error adding your datums')
@@ -36,10 +27,12 @@
     }
 
     insights.submitSelection = function(selection) {
-      console.log("submitSelection selection", selection)
+      
       insights.getCorrelations(selection)
       .then(function(dataArr){
-          return dataArr;
+        console.log('This is the correl data', dataArr)
+          store.set('currentCorrelationData', dataArr);
+          insights.currentCorrelationData = store.get('dataArr');
       })
       .catch(function(err){
         console.log("There was an error getting your correlations friendo!", err);
