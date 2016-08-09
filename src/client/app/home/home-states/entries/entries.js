15760d34e50483c1a0233e919cc179d37a55f6bb
doitlikepruett <doitlikepruett@gmail.com>
2016-08-09 15:01:54 -0700

    (fix) Correct the incorrect firstname lastname setting on post req

--- src/client/app/home/home-states/entries/entries.js
+++ src/client/app/home/home-states/entries/entries.js
@@ -3,18 +3,24 @@
   angular.module('app.home.entries', []).controller('EntriesController',
     EntriesController);
 
-  function EntriesController($http, $scope, homeFactory) {
+  function EntriesController($http, $scope, homeFactory, store) {
     var entries = this;
+
     entries.userDate;
+
     entries.autoCompleteDisabled = true;
+
     entries.changeAutoComplete = function() {
       entries.userDate ? entries.autoCompleteDisabled = false : entries.autoCompleteDisabled =
         true;
     }
+
     entries.daysActivities = [];
     entries.daysMetrics = [];
-    entries.userActivities = JSON.parse(window.localStorage.userData).userActivities
-    entries.userMetrics = JSON.parse(window.localStorage.userData).userMetrics
+
+    entries.userActivities = store.get('profile').userActivities;
+    entries.userMetrics = store.get('profile').userMetrics;
+
     entries.userActivitiesStub = ['run', 'rage', 'slide', 'skip', 'hop','hold', 'hodor'];
     entries.userMetricsStub = ['headache', 'angry', 'happy', 'sad','joyful'];
 
@@ -29,6 +35,7 @@
         entries.metricForm.$setPristine();
       }
     }
+
     entries.removeItem = function(index, type) {
       if (type === 'activity') {
         entries.daysActivities.splice(index, 1);
@@ -36,6 +43,7 @@
         entries.daysMetrics.splice(index, 1);
       }
     }
+
     entries.postData = function(type) {
       var url = type === 'activity' ? '/user/activity' : '/user/metric';
       var datums = type === 'activity' ? entries.daysActivities : entries
@@ -45,8 +53,8 @@
         url: url,
         data: {
           email: window.localStorage.userData.email,
-          firstname: window.localStorage.userData.given_name,
-          lastname: window.localStorage.userData.family_name,
+          firstname: store.get('profile').given_name,
+          lastname: store.get('profile').family_name,
           datums: datums,
           date: entries.userDate
         }
@@ -59,5 +67,6 @@
         alert('Sorry, there was an error adding your datums')
       })
     }
+
   }
 })();
