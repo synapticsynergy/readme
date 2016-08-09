19de340d76d9d44d75d24f5136ac50b1fd0d7633
budleigh <arguewithplato@gmail.com>
2016-08-08 17:04:00 -0700

    (feat) Create journal factory/controller

--- src/client/app/home/home-states/journal/journal.js
+++ src/client/app/home/home-states/journal/journal.js
@@ -1,10 +1,41 @@
 (function() {
   'use strict';
-  angular.module('app.home.journal', []).controller('JournalController',
-    JournalController);
+  angular.module('app.home.journal', [])
+  .controller('JournalController', JournalController)
+  .factory('Journal', Journal);
 
-  function JournalController($scope) {
+  function JournalController($scope, Journal) {
     /*jshint validthis: true */
     var journal = this;
+    journal.dailyMemo = '';
+    journal.message = '';
+    journal.bounceTimeout = null;
+
+    journal.onChange = function () {
+      journal.message = 'edited...';
+      clearTimeout(journal.bounceTimeout);
+      journal.bounceTimeout = setTimeout(function () {
+        journal.save();
+        journal.message = 'saved!';
+      }, 2000);
+    }
+
+    journal.save = function () {
+      Journal.saveJournal(journal.dailyMemo, new Date().getDate().toString());
+    }
+  }
+
+  function Journal ($http) {
+    var services = {};
+
+    services.saveJournal = function (text, day) {
+      return $http.post({
+        url: '/user/journal',
+        entry: text,
+        day: day.toString()
+      });
+    }
+
+    return services;
   }
 })();
