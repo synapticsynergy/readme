722822840a9ecebaefd6ac5815d51424f8a8bf6e
budleigh <arguewithplato@gmail.com>
2016-08-09 22:09:54 -0700

    (chore) Lint home.js

--- src/client/app/home/home.js
+++ src/client/app/home/home.js
@@ -1,30 +1,30 @@
-(function() {
+(function () {
   'use strict';
   angular.module('app.home', ['app.home.entries', 'app.home.insights',
     'app.home.journal'
   ]).controller('HomeController', HomeController).factory('homeFactory',
     homeFactory);
 
-  function HomeController($scope) {
-    /*jshint validthis: true */
+  function HomeController () {
+    // jshint validthis: true
     var home = this;
   }
 
-  function homeFactory($http, store) {
+  function homeFactory ($http, store) {
     var services = {};
-    services.getUserData = function() {
+    services.getUserData = function () {
       var profile = store.get('profile');
       return $http({
-        method: "POST",
+        method: 'POST',
         url: '/user',
         data: {
           email: profile.email,
           firstname: profile.given_name,
           lastname: profile.family_name
         }
-      }).then(function(returnedData) {
+      }).then(function (returnedData) {
         store.set('userData', returnedData.data);
-      })
+      });
     };
 
     services.getDay = function (date) {
