4f0aa5347de812eca765f058c56fc1979ec84abc
Blake Danson <blakedanson@gmail.com>
2016-08-11 19:09:19 -0700

    (feat) Add svg container to insights view

--- src/client/app/home/home-states/insights/insightsBubbles.directive.js
+++ src/client/app/home/home-states/insights/insightsBubbles.directive.js
@@ -26,18 +26,25 @@ angular.module('app.home.insights').
            //in D3, any selection[0] contains the group
            //selection[0][0] is the DOM node
            //but we won't need that this time
-           var chart = d3.select(element[0]);
+           var svgContainer = d3.select(element[0]);
            //to our original directive markup bars-chart
            //we add a div with out chart stling and bind each
            //data entry to the chart
-            chart.append("div")
-            .attr("class", "chart")
-             .selectAll("div")
-             .data(data).enter().append("div")
-             // .transition().duration(1000).ease("elastic")
-             .style("width", function(d) { return d + "%"; })
-             .style('background-color','blue')
-             .text(function(d) { return d + "%"; });
+            var svg = svgContainer.append("svg").attr('width', 100 + '%').attr('height','500px');
+
+            svg.attr("class", "svgContainer");
+
+
+
+
+
+
+             // .selectAll("svg")
+             // .data(data).enter().append("circle")
+             // // .transition().duration(1000).ease("elastic")
+             // .style("width", function(d) { return d + "%"; })
+             // .style('background-color','blue')
+             // .text(function(d) { return d + "%"; });
            //a little of magic: setting it's width based
            //on the data value (d)
            //and text all with a smooth transition
