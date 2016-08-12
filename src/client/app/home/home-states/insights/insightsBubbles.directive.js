693562401d0350c7038215f17119ff0edd5b80bf
Blake Danson <blakedanson@gmail.com>
2016-08-12 11:30:25 -0700

    (feat) Add gravitational nodes in insights

--- src/client/app/home/home-states/insights/insightsBubbles.directive.js
+++ src/client/app/home/home-states/insights/insightsBubbles.directive.js
@@ -21,7 +21,7 @@ angular.module('app.home.insights').
          replace: false,
          link: function (scope, element, attrs) {
            //converting all data passed thru into an array
-           var data = attrs.chartData.split(',');
+           var data = scope.data;
            console.log(data,'d3');
            //in D3, any selection[0] contains the group
            //selection[0][0] is the DOM node
@@ -29,13 +29,78 @@ angular.module('app.home.insights').
            var svgContainer = d3.select(element[0]);
            //to our original directive markup bars-chart
            //we add a div with out chart stling and bind each
+
+           var width = 640;
+           var height = 480;
+
            //data entry to the chart
-            var svg = svgContainer.append("svg").attr('width', 100 + '%').attr('height','500px');
+            var svg = svgContainer.append("svg").attr('width', width).attr('height', height);
 
             svg.attr("class", "svgContainer");
 
 
+            var links = [
+            { source: 'Baratheon', target:'Lannister' },
+            { source: 'Baratheon', target:'Stark' },
+            { source: 'Lannister', target:'Stark' },
+            { source: 'Stark', target:'Bolton' }
+            ];
 
+
+            // create empty nodes array
+            var nodes = {};
+
+            // compute nodes from links data
+            links.forEach(function(link) {
+                link.source = nodes[link.source] ||
+                    (nodes[link.source] = {name: link.source});
+                link.target = nodes[link.target] ||
+                    (nodes[link.target] = {name: link.target});
+            });
+
+
+
+
+
+
+            // use the force
+            var force = d3.layout.force() //build the layout
+                .size([width, height]) //specified earlier
+                .nodes(d3.values(nodes)) //add nodes
+                // .links(links) //add links
+                .on("tick", tick) //what to do
+                .linkDistance(300) //set for proper svg size
+                .start(); //kick the party off!
+
+
+
+                // add the links
+                var link = svg.selectAll('.link')
+                    .data(links)
+                    .enter().append('line')
+                    .attr('class', 'link');
+
+                // add the nodes
+                var node = svg.selectAll('.node')
+                    .data(force.nodes()) //add
+                    .enter().append('circle')
+                    .attr('class', 'node')
+                    .attr('r', width * 0.03); //radius of circle
+
+
+
+              function tick(e) {
+
+                  node.attr('cx', function(d) { return d.x; })
+                      .attr('cy', function(d) { return d.y; })
+                      .call(force.drag);
+
+                  link.attr('x1', function(d) { return d.source.x; })
+                      .attr('y1', function(d) { return d.source.y; })
+                      .attr('x2', function(d) { return d.target.x; })
+                      .attr('y2', function(d) { return d.target.y; });
+
+              }
 
 
 
@@ -48,7 +113,8 @@ angular.module('app.home.insights').
            //a little of magic: setting it's width based
            //on the data value (d)
            //and text all with a smooth transition
-         }
+         },
+         scope: { data: '=' }
       };
       return directiveDefinitionObject;
    };
