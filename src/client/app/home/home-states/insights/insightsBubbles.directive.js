4492e59ba0ef5dbfe500a667ad7086a41010c419
Blake Danson <blakedanson@gmail.com>
2016-08-12 17:28:22 -0700

    (feat) Add labels to circles

--- src/client/app/home/home-states/insights/insightsBubbles.directive.js
+++ src/client/app/home/home-states/insights/insightsBubbles.directive.js
@@ -21,7 +21,13 @@ angular.module('app.home.insights').
 
         //converting all data passed thru into an array of objects.
         var data = store.get('currentCorrelationData');
-        console.log(data,'d3');
+        data = data.map(function(obj) {
+          var result = {};
+          result.label = Object.keys(obj)[0];
+          result.r = obj[result.label] * 100 + 1;
+          return result;
+        });
+        console.log(data[0],'d3');
         //in D3, any selection[0] contains the group
 
         var svgContainer = d3.select(element[0]);
@@ -37,30 +43,91 @@ angular.module('app.home.insights').
         svg.attr("class", "svgContainer");
 
 
+        //text data
+
+        var nodes = d3.values(data);
+
         // layout for gravitational effect.
         var force = d3.layout.force() //build the layout
           .size([width, height]) //specified earlier
-          .nodes(d3.values(data)) //add nodes
+          .nodes(nodes) //add nodes
           .on("tick", tick) //what to do
-          .linkDistance(300) //set for proper svg size
           .start();
 
 
         // add the nodes
         var node = svg.selectAll('.node')
           .data(force.nodes()) //add
-          .enter().append('circle')
+
+        var elemEnter = node.enter().append('g')
+            .attr("transform", function(d){return "translate("+d.x+",80)"});
+
+
+        /*Create the circle for each block */
+        var circle = elemEnter.append("circle")
           .attr('class', 'node')
-          .attr('r', width * 0.03)//radius of circle
-          .style('fill','lightblue');
+          .attr("r", function(d){return d.r} )
+          .attr("stroke","black")
+          .attr("fill", "white");
+
+        /* Create the text for each block */
+        elemEnter.append("text")
+          .attr("dr", function(d){return -20})
+          .text(function(d){return d.label});
 
 
+
+
+        force.on("tick", function(e) {
+          var q = d3.geom.quadtree(nodes),
+              i = 0,
+              n = nodes.length;
+
+          while (++i < n) q.visit(collide(nodes[i]));
+
+          svg.selectAll("g")
+              .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+              .call(force.drag)
+        });
+
+
+        function collide(node) {
+          var r = node.r + 16,
+              nx1 = node.x - r,
+              nx2 = node.x + r,
+              ny1 = node.y - r,
+              ny2 = node.y + r;
+          return function(quad, x1, y1, x2, y2) {
+            if (quad.point && (quad.point !== node)) {
+              var x = node.x - quad.point.x,
+                  y = node.y - quad.point.y,
+                  l = Math.sqrt(x * x + y * y),
+                  r = node.radius + quad.point.radius;
+              if (l < r) {
+                l = (l - r) / l * .5;
+                node.x -= x *= l;
+                node.y -= y *= l;
+                quad.point.x += x;
+                quad.point.y += y;
+              }
+            }
+            return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+          };
+        }
+
+
+        //semi works
+        //   .enter().append('circle')
+        //   .attr('class', 'node')
+        //   .attr('r', width * 0.03)//radius of circle
+        //   .style('fill','lightblue')
+        //   .attr('stroke','grey')
 
         function tick(e) {
 
-          node.attr('cx', function(d) { return d.x; })
-              .attr('cy', function(d) { return d.y; })
-              .call(force.drag);
+          node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+            .call(force.drag);
+
 
         }
 
