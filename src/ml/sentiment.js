47b6c318c91166c84fa34344c1438d75519f87ee
budleigh <arguewithplato@gmail.com>
2016-08-17 22:27:26 -0700

    (feat) Add sentiment controller

---
+++ src/ml/sentiment.js
@@ -0,0 +1,26 @@
+const Alchemy = require('./vendor/alchemyapi');
+const api = new Alchemy();
+
+exports.getSentiment = function (text) {
+  return new Promise(function (resolve) {
+    api.sentiment('text', text, {}, function (res) {
+      resolve(res);
+    });
+  });
+};
+
+exports.getConcepts = function (text) {
+  return new Promise(function (resolve) {
+    api.concepts('text', text, { showSourceText: 1 }, function (res) {
+      resolve(res);
+    });
+  });
+};
+
+exports.getKeywords = function (text) {
+  return new Promise(function (resolve) {
+    api.keywords('text', text, { sentiment: 1 }, function (res) {
+      resolve(res);
+    });
+  });
+};
